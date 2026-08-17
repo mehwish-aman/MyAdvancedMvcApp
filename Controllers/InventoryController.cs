311b9ca7648a435bc387eaaf284a832e6cd3dbf8
mehwish-aman <mehwishaman0@gmail.com>
2026-08-17 11:48:59 +0500

    Updated Layouts and clicks

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -9,12 +9,16 @@ public class InventoryController: Controller
         return View();
     }
     //jab save ho to yeh ho
-    [HttpPost]
 //database nhi hai abhi is liye srf conformation ayegi abhi 
 //r hm model ka data hai usy hm confirm view ko b bhj rhy hain taky 
 //wahan b use ho sakey 
-    public IActionResult Save(InventoryItem model)
-    {
-        return View("confirm",model);
-    }
+   [HttpPost]
+   public IActionResult Save(InventoryItem model)
+   {
+    // Database nahi hai abhi, isliye sirf success signal bhej rahe hain
+    // Poora HTML page return nahi kar rahe, kyunki JavaScript ko sirf confirmation chahiye 
+    //so js srf yeh message dikhayega
+    return Ok(new { message = "Item received successfully (not saved to database yet)" });
+   }
+    
 }
--- obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df1913e0243d8213ba7a08d88cd9c8efb234188")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
