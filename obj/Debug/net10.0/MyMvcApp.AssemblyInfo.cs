b50a7e92cc0dc796a8260d72722190931943acd4
mehwish-aman <mehwishaman0@gmail.com>
2026-08-19 15:55:58 +0500

    Your descriptive commit message

--- obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32877a707d25828e1f223db3be7e622b5d91d976")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f3b139d50cdb826ed0a1c1301230c5450b57781")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
